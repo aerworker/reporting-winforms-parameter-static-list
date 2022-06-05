06a9499b7e1a10354ca5571cfcb0edf696925079
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2022-06-05 15:02:06 -0700

    auto sync for version 2022.1

---
+++ CS/Form1.cs
@@ -0,0 +1,77 @@
+using DevExpress.XtraReports.Parameters;
+using DevExpress.XtraReports.UI;
+using System.Windows.Forms;
+using System;
+using System.IO;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
+
+namespace Reporting_Create_Report_Parameter_with_Predefined_Static_Values {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void configureDataSource(ref XtraReport1 report) {
+            var projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            var databasePath = Path.Combine(projectDirectory, "nwind.db");
+            var connectionParameters = new SQLiteConnectionParameters(databasePath, "");
+            var dataSource = new SqlDataSource(connectionParameters);
+
+            var ordersQuery = new CustomSqlQuery();
+            ordersQuery.Name = "Orders";
+            ordersQuery.Sql = "SELECT * FROM Orders";
+
+            dataSource.Queries.Add(ordersQuery);
+
+            report.DataSource = dataSource;
+            report.DataMember = "Orders";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create a date-time parameter.
+            Parameter dateParameter = new Parameter();
+            dateParameter.Name = "dateParameter";
+            dateParameter.Description = "Date:";
+            dateParameter.Type = typeof(System.DateTime);
+
+            // Create a StaticListLookUpSettings instance and add predefined
+            // static values to the instance's LookUpValues collection.
+            StaticListLookUpSettings lookupSettings = new StaticListLookUpSettings();
+
+            lookupSettings.LookUpValues.Add(new LookUpValue
+            {
+                Value = new DateTime(2014, 01, 01),
+                Description = "January 1, 2014"
+            }
+            );
+
+            lookupSettings.LookUpValues.Add(new LookUpValue
+            {
+                Value = new DateTime(2015, 02, 01),
+                Description = "February 1, 2015"
+            }
+            );
+
+            lookupSettings.LookUpValues.Add(new LookUpValue
+            {
+                Value = new DateTime(2016, 03, 01),
+                Description = "March 1, 2016"
+            }
+            );
+
+            // Assign the specified settings to the parameter's ValueSourceSettings property.
+            dateParameter.ValueSourceSettings = lookupSettings;
+
+            // Create a report instance and add the parameter
+            // to the report's Parameters collection.
+            XtraReport1 report = new XtraReport1();
+            report.Parameters.Add(dateParameter);
+
+            // Use the created parameter to filter the report's data.
+            report.FilterString = "GetDate([OrderDate]) >= ?dateParameter";
+            configureDataSource(ref report);
+            report.ShowPreview();
+        }
+    }
+}
---
+++ VB/XtraReport1.Designer.vb
@@ -0,0 +1,124 @@
+﻿Namespace Reporting_Create_Report_Parameter_with_Predefined_Static_Values
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.xrTable1 = New DevExpress.XtraReports.UI.XRTable()
+			Me.xrTableRow1 = New DevExpress.XtraReports.UI.XRTableRow()
+			Me.xrTableCell1 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell2 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrLabel2 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+			DirectCast(Me.xrTable1, System.ComponentModel.ISupportInitialize).BeginInit()
+			DirectCast(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' TopMargin
+			' 
+			Me.TopMargin.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrTable1})
+			Me.TopMargin.Name = "TopMargin"
+			' 
+			' xrTable1
+			' 
+			Me.xrTable1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 75F)
+			Me.xrTable1.Name = "xrTable1"
+			Me.xrTable1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F)
+			Me.xrTable1.Rows.AddRange(New DevExpress.XtraReports.UI.XRTableRow() { Me.xrTableRow1})
+			Me.xrTable1.SizeF = New System.Drawing.SizeF(650F, 25F)
+			' 
+			' xrTableRow1
+			' 
+			Me.xrTableRow1.Cells.AddRange(New DevExpress.XtraReports.UI.XRTableCell() { Me.xrTableCell1, Me.xrTableCell2})
+			Me.xrTableRow1.Name = "xrTableRow1"
+			Me.xrTableRow1.Weight = 11.5R
+			' 
+			' xrTableCell1
+			' 
+			Me.xrTableCell1.Multiline = True
+			Me.xrTableCell1.Name = "xrTableCell1"
+			Me.xrTableCell1.Text = "Customer ID"
+			Me.xrTableCell1.Weight = 0.22222222222222221R
+			' 
+			' xrTableCell2
+			' 
+			Me.xrTableCell2.Multiline = True
+			Me.xrTableCell2.Name = "xrTableCell2"
+			Me.xrTableCell2.Text = "Order Date"
+			Me.xrTableCell2.Weight = 0.22222222222222221R
+			' 
+			' BottomMargin
+			' 
+			Me.BottomMargin.Name = "BottomMargin"
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel2, Me.xrLabel1})
+			Me.Detail.HeightF = 28.125F
+			Me.Detail.Name = "Detail"
+			' 
+			' xrLabel2
+			' 
+			Me.xrLabel2.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[OrderDate]")})
+			Me.xrLabel2.LocationFloat = New DevExpress.Utils.PointFloat(325F, 0F)
+			Me.xrLabel2.Multiline = True
+			Me.xrLabel2.Name = "xrLabel2"
+			Me.xrLabel2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel2.SizeF = New System.Drawing.SizeF(325F, 28.125F)
+			Me.xrLabel2.Text = "xrLabel2"
+			Me.xrLabel2.TextFormatString = "{0:d}"
+			' 
+			' xrLabel1
+			' 
+			Me.xrLabel1.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[CustomerID]")})
+			Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 0F)
+			Me.xrLabel1.Multiline = True
+			Me.xrLabel1.Name = "xrLabel1"
+			Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel1.SizeF = New System.Drawing.SizeF(325F, 28.125F)
+			Me.xrLabel1.Text = "xrLabel1"
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.TopMargin, Me.BottomMargin, Me.Detail})
+			Me.Font = New System.Drawing.Font("Arial", 9.75F)
+			Me.Version = "21.1"
+			DirectCast(Me.xrTable1, System.ComponentModel.ISupportInitialize).EndInit()
+			DirectCast(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+		Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private xrTable1 As DevExpress.XtraReports.UI.XRTable
+		Private xrTableRow1 As DevExpress.XtraReports.UI.XRTableRow
+		Private xrTableCell1 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell2 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrLabel2 As DevExpress.XtraReports.UI.XRLabel
+		Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+	End Class
+End Namespace
