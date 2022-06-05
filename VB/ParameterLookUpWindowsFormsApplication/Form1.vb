fae9a3e01663d1358b75976a12cb1d67a1619b8e
DevExpressExampleBot <fakeEmail>
2022-06-05 22:55:22 +0400

    Source auto update [skip ci]

--- VB/ParameterLookUpWindowsFormsApplication/Form1.vb
+++ VB/ParameterLookUpWindowsFormsApplication/Form1.vb
@@ -1,49 +1,40 @@
-﻿Imports DevExpress.XtraReports.Parameters
+Imports DevExpress.XtraReports.Parameters
 Imports DevExpress.XtraReports.UI
 Imports System
 Imports System.Windows.Forms
-' ...
 
+' ...
 Namespace ParameterLookUpWindowsFormsApplication
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
             ' Create a report instance.
-            Dim report As New XtraReport1()
-
+            Dim report As XtraReport1 = New XtraReport1()
             ' Create a multi-value parameter and specify its name.
-            Dim parameter1 As New Parameter()
+            Dim parameter1 As Parameter = New Parameter()
             parameter1.MultiValue = True
             parameter1.Name = "parameterCategory"
-
             ' Specify other parameter properties.
-            parameter1.Type = GetType(System.Int32)
+            parameter1.Type = GetType(Integer)
             parameter1.Visible = True
             parameter1.Description = "Category Name: "
-
             ' Populate the parameter's lookup editor with a set of values.
             parameter1.LookUpSettings = New StaticListLookUpSettings()
-            CType(parameter1.LookUpSettings, StaticListLookUpSettings).LookUpValues.AddRange(New LookUpValue() { _
-                New LookUpValue(1, "Beverages"), _
-                New LookUpValue(2, "Confections"), _
-                New LookUpValue(3, "Grains/Cereals") _
-            })
+            CType(parameter1.LookUpSettings, StaticListLookUpSettings).LookUpValues.AddRange(New LookUpValue() {New LookUpValue(1, "Beverages"), New LookUpValue(2, "Confections"), New LookUpValue(3, "Grains/Cereals")})
             ' Assign the default values to the parameter.
-            parameter1.Value = New Integer() { 1, 2 }
-
+            parameter1.Value = New Integer() {1, 2}
             ' Add the parameter to the report.
             report.Parameters.Add(parameter1)
-
             ' Specify the report's filter string.
             report.FilterString = "[CategoryID] In (?parameterCategory)"
-
             ' Load the report's Print Preview.
-            Dim pt As New ReportPrintTool(report)
+            Dim pt As ReportPrintTool = New ReportPrintTool(report)
             pt.ShowPreviewDialog()
         End Sub
     End Class
--- VB/ParameterLookUpWindowsFormsApplication/XtraReport1.Designer.vb
+++ VB/ParameterLookUpWindowsFormsApplication/XtraReport1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace ParameterLookUpWindowsFormsApplication
-    Partial Public Class XtraReport1
+Namespace ParameterLookUpWindowsFormsApplication
+
+    Partial Class XtraReport1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,27 +12,27 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Designer generated code"
-
+#Region "Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
             Me.components = New System.ComponentModel.Container()
-            Dim tableQuery1 As New DevExpress.DataAccess.Sql.TableQuery()
-            Dim tableInfo1 As New DevExpress.DataAccess.Sql.TableInfo()
-            Dim columnInfo1 As New DevExpress.DataAccess.Sql.ColumnInfo()
-            Dim columnInfo2 As New DevExpress.DataAccess.Sql.ColumnInfo()
-            Dim columnInfo3 As New DevExpress.DataAccess.Sql.ColumnInfo()
-            Dim columnInfo4 As New DevExpress.DataAccess.Sql.ColumnInfo()
-            Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(XtraReport1))
+            Dim tableQuery1 As DevExpress.DataAccess.Sql.TableQuery = New DevExpress.DataAccess.Sql.TableQuery()
+            Dim tableInfo1 As DevExpress.DataAccess.Sql.TableInfo = New DevExpress.DataAccess.Sql.TableInfo()
+            Dim columnInfo1 As DevExpress.DataAccess.Sql.ColumnInfo = New DevExpress.DataAccess.Sql.ColumnInfo()
+            Dim columnInfo2 As DevExpress.DataAccess.Sql.ColumnInfo = New DevExpress.DataAccess.Sql.ColumnInfo()
+            Dim columnInfo3 As DevExpress.DataAccess.Sql.ColumnInfo = New DevExpress.DataAccess.Sql.ColumnInfo()
+            Dim columnInfo4 As DevExpress.DataAccess.Sql.ColumnInfo = New DevExpress.DataAccess.Sql.ColumnInfo()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ParameterLookUpWindowsFormsApplication.XtraReport1))
             Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
             Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
             Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
@@ -38,11 +40,11 @@
             Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
             Me.xrLabel3 = New DevExpress.XtraReports.UI.XRLabel()
             Me.xrPictureBox1 = New DevExpress.XtraReports.UI.XRPictureBox()
-            DirectCast(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).BeginInit()
             ' 
             ' Detail
             ' 
-            Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrPictureBox1, Me.xrLabel3, Me.xrLabel1})
+            Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() {Me.xrPictureBox1, Me.xrLabel3, Me.xrLabel1})
             Me.Detail.HeightF = 91.66666F
             Me.Detail.Name = "Detail"
             Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
@@ -72,14 +74,14 @@
             columnInfo2.Name = "CategoryName"
             columnInfo3.Name = "Description"
             columnInfo4.Name = "Picture"
-            tableInfo1.SelectedColumns.AddRange(New DevExpress.DataAccess.Sql.ColumnInfo() { columnInfo1, columnInfo2, columnInfo3, columnInfo4})
-            tableQuery1.Tables.AddRange(New DevExpress.DataAccess.Sql.TableInfo() { tableInfo1})
-            Me.sqlDataSource1.Queries.AddRange(New DevExpress.DataAccess.Sql.SqlQuery() { tableQuery1})
+            tableInfo1.SelectedColumns.AddRange(New DevExpress.DataAccess.Sql.ColumnInfo() {columnInfo1, columnInfo2, columnInfo3, columnInfo4})
+            tableQuery1.Tables.AddRange(New DevExpress.DataAccess.Sql.TableInfo() {tableInfo1})
+            Me.sqlDataSource1.Queries.AddRange(New DevExpress.DataAccess.Sql.SqlQuery() {tableQuery1})
             Me.sqlDataSource1.ResultSchemaSerializable = resources.GetString("sqlDataSource1.ResultSchemaSerializable")
             ' 
             ' xrLabel1
             ' 
-            Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Categories.CategoryName")})
+            Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() {New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Categories.CategoryName")})
             Me.xrLabel1.Font = New System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold)
             Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(23.95833F, 10.75001F)
             Me.xrLabel1.Name = "xrLabel1"
@@ -90,7 +92,7 @@
             ' 
             ' xrLabel3
             ' 
-            Me.xrLabel3.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Categories.Description")})
+            Me.xrLabel3.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() {New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Categories.Description")})
             Me.xrLabel3.Font = New System.Drawing.Font("Tahoma", 9.75F)
             Me.xrLabel3.LocationFloat = New DevExpress.Utils.PointFloat(23.95833F, 48.91666F)
             Me.xrLabel3.Name = "xrLabel3"
@@ -101,7 +103,7 @@
             ' 
             ' xrPictureBox1
             ' 
-            Me.xrPictureBox1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Image", Nothing, "Categories.Picture")})
+            Me.xrPictureBox1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() {New DevExpress.XtraReports.UI.XRBinding("Image", Nothing, "Categories.Picture")})
             Me.xrPictureBox1.LocationFloat = New DevExpress.Utils.PointFloat(507.2917F, 10.75001F)
             Me.xrPictureBox1.Name = "xrPictureBox1"
             Me.xrPictureBox1.SizeF = New System.Drawing.SizeF(119.7917F, 61.16665F)
@@ -109,23 +111,27 @@
             ' 
             ' XtraReport1
             ' 
-            Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.TopMargin, Me.BottomMargin})
-            Me.ComponentStorage.AddRange(New System.ComponentModel.IComponent() { Me.sqlDataSource1})
+            Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() {Me.Detail, Me.TopMargin, Me.BottomMargin})
+            Me.ComponentStorage.AddRange(New System.ComponentModel.IComponent() {Me.sqlDataSource1})
             Me.DataMember = "Categories"
             Me.DataSource = Me.sqlDataSource1
             Me.Version = "15.2"
-            DirectCast(Me, System.ComponentModel.ISupportInitialize).EndInit()
-
+            CType((Me), System.ComponentModel.ISupportInitialize).EndInit()
         End Sub
 
-        #End Region
-
+#End Region
         Private Detail As DevExpress.XtraReports.UI.DetailBand
+
         Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+
         Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+
         Private sqlDataSource1 As DevExpress.DataAccess.Sql.SqlDataSource
+
         Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+
         Private xrPictureBox1 As DevExpress.XtraReports.UI.XRPictureBox
+
         Private xrLabel3 As DevExpress.XtraReports.UI.XRLabel
     End Class
 End Namespace
