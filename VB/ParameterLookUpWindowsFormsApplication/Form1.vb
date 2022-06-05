86a492847425d5adea1c2d246fda7849097eb381
DevExpressExampleBot <fakeEmail>
2022-06-05 22:58:06 +0400

    Source auto update [skip ci]

--- VB/ParameterLookUpWindowsFormsApplication/Form1.vb
+++ VB/ParameterLookUpWindowsFormsApplication/Form1.vb
@@ -1,52 +1,41 @@
-﻿Imports DevExpress.XtraReports.Parameters
+Imports DevExpress.XtraReports.Parameters
 Imports DevExpress.XtraReports.UI
 Imports System
 Imports System.Windows.Forms
+
 ' ...
-
 Namespace ParameterLookUpWindowsFormsApplication
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			If Not Me.button1.IsHandleCreated Then Return
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			' Create a report instance.
-			Dim report As New XtraReport1()
-
-			' Create a multi-value report parameter and specify its name.
-			Dim parameter1 As New Parameter()
-			parameter1.MultiValue = True
-			parameter1.Name = "parameterCategory"
-
-			' Specify other parameter properties.
-			parameter1.Type = GetType(System.Int32)
-			parameter1.Visible = True
-			parameter1.Description = "Category Name: "
-
-			' Populate the parameter's lookup editor with a set of values.
-			parameter1.LookUpSettings = New StaticListLookUpSettings()
-			CType(parameter1.LookUpSettings, StaticListLookUpSettings).LookUpValues.AddRange(New LookUpValue() {
-				New LookUpValue(1, "Beverages"),
-				New LookUpValue(2, "Condiments"),
-				New LookUpValue(3, "Confections")
-			})
-			' Assign the default values to the parameter.
-			parameter1.Value = New Integer() { 1, 2 }
-
-			' Add the parameter to the report.
-			report.Parameters.Add(parameter1)
-
-			' Specify the report's filter string.
-			report.FilterString = "[CategoryID] In (?parameterCategory)"
-
-			' Load the report's Print Preview.
-			Dim pt As New ReportPrintTool(report)
-			pt.ShowPreviewDialog()
-		End Sub
-	End Class
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            ' Create a report instance.
+            Dim report As XtraReport1 = New XtraReport1()
+            ' Create a multi-value report parameter and specify its name.
+            Dim parameter1 As Parameter = New Parameter()
+            parameter1.MultiValue = True
+            parameter1.Name = "parameterCategory"
+            ' Specify other parameter properties.
+            parameter1.Type = GetType(Integer)
+            parameter1.Visible = True
+            parameter1.Description = "Category Name: "
+            ' Populate the parameter's lookup editor with a set of values.
+            parameter1.LookUpSettings = New StaticListLookUpSettings()
+            CType(parameter1.LookUpSettings, StaticListLookUpSettings).LookUpValues.AddRange(New LookUpValue() {New LookUpValue(1, "Beverages"), New LookUpValue(2, "Condiments"), New LookUpValue(3, "Confections")})
+            ' Assign the default values to the parameter.
+            parameter1.Value = New Integer() {1, 2}
+            ' Add the parameter to the report.
+            report.Parameters.Add(parameter1)
+            ' Specify the report's filter string.
+            report.FilterString = "[CategoryID] In (?parameterCategory)"
+            ' Load the report's Print Preview.
+            Dim pt As ReportPrintTool = New ReportPrintTool(report)
+            pt.ShowPreviewDialog()
+        End Sub
+    End Class
 End Namespace
